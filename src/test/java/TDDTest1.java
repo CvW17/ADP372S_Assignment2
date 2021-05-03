2c6c5a162d5c9467acff16e82e245de4668e79f1
Cameron van Wyk <219088470@mycput.ac.za>
2021-05-03 16:58:54 +0200

    Created extra test class

---
+++ src/test/java/TDDTest1.java
@@ -0,0 +1,21 @@
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TDDTest1
+{
+
+    @BeforeEach
+    void setUp()
+    {
+
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+
+    }
+}
