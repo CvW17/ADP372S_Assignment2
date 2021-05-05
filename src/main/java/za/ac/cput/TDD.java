54a971f153bc7b9e0651f171835c48539ba7e88d
Cameron van Wyk <219088470@mycput.ac.za>
2021-05-05 10:26:14 +0200

    Added list add and remove test

--- src/main/java/za/ac/cput/TDD.java
+++ src/main/java/za/ac/cput/TDD.java
@@ -1,6 +1,24 @@
 package za.ac.cput;
 
+import java.util.*;
+
 public class TDD
 {
+    private List<String> list=new ArrayList<String>();
 
+    public void addList(String add)
+    {
+        list.clear();
+        list.add(add);
+    }
+
+    public int getListSize()
+    {
+        return list.size();
+    }
+
+    public void clearList()
+    {
+        list.remove(0);
+    }
 }
--- src/test/java/za/ac/cput/TDDTest.java
+++ src/test/java/za/ac/cput/TDDTest.java
@@ -1,19 +1,44 @@
 package za.ac.cput;
 
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TDDTest
 {
+    TDD tdd = new TDD();
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp()
+    public boolean ListAdd()
     {
 
+        int Size = tdd.getListSize();
+        tdd.addList("Zero");
+
+        if (Size != tdd.getListSize())
+            return true;
+        else return false;
     }
 
-    @org.junit.jupiter.api.AfterEach
-    void tearDown()
+    @Test
+    void testListAdd()
     {
+        assertEquals(true, this.ListAdd());
+    }
 
+    public boolean ListRemove()
+    {
+        int Size = tdd.getListSize();
+        tdd.addList("Zero");
+
+        tdd.clearList();
+        if (Size == tdd.getListSize())
+            return true;
+        else return false;
+    }
+
+    @Test
+    void TestListRemove()
+    {
+        assertEquals(true, this.ListRemove());
     }
 }
